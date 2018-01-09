bc8323960685fbc77467f5493f23aa3b14ea24a9
Balasivaguru <34646489+Balasivaguru@users.noreply.github.com>
2018-01-09 10:13:37 +0000

    Print first repeating number

---
+++ Hunter/Set_1/6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+	int a[10],i,j,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n-1;i++)
+	for(j=i+1;j<n;j++)
+	if(a[i]==a[j])
+	{
+	printf("%d ",a[i]);
+	return 0;
+	}
+}
