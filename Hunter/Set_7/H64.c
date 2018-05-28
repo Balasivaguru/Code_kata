bfebc6c4c3148cb001cbd15d66dd9d53c3ae0e5b
Balasivaguru <34646489+Balasivaguru@users.noreply.github.com>
2018-05-28 07:10:22 +0530

    Create H64.c

---
+++ Hunter/Set_7/H64.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,a,b,c,d,e,f;
+	scanf("%d",&n);
+	a=n%1000;
+	b=a%500;
+	c=b%100;
+	d=c%50;
+	e=d%10;
+	f=e%1;
+    printf("%d",(n/1000)+(a/500)+(b/100)+(c/50)+(d/10)+(e/1));
+}
+
